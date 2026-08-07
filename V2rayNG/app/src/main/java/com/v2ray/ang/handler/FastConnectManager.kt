042e1c41ef76400bc1e5c4276cfbccaeaddd98bb
Saw San <3818891+sawsan23@users.noreply.github.com>
2026-08-07 14:59:21 +0630

    Update FastConnectManager.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/handler/FastConnectManager.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/handler/FastConnectManager.kt
@@ -1,5 +1,6 @@
 package com.v2ray.ang.handler
 
+import kotlinx.coroutines.delay
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.handler.MmkvManager.decodeAllServerList
 import com.v2ray.ang.handler.MmkvManager.decodeServerConfig
